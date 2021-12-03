b3ad73a1aa5523136657553ca2684af5a6220f55
Michael <hanaauana@gmail.com>
2021-12-02 19:10:08 -0800

    Complete day 1 part 2

--- 2021/day1.py
+++ 2021/day1.py
@@ -1,5 +1,6 @@
 input_filename = 'day1-input.txt'
 
+# Part 1
 increase_counter = 0
 with open(input_filename) as input_file:
     prev_line = None
@@ -10,4 +11,23 @@ with open(input_filename) as input_file:
                 increase_counter += 1
         prev_line = cur_line
 
+print(increase_counter)
+
+
+# Part 2
+increase_counter = 0
+with open(input_filename) as input_file:
+    prev_line_2 = None
+    prev_line_1 = None
+    prev_sum = None
+    for line in input_file:
+        cur_line = int(line)
+        if prev_line_1 and prev_line_2:
+            cur_sum = cur_line + prev_line_1 + prev_line_2
+            if prev_sum:
+                if cur_sum > prev_sum:
+                    increase_counter += 1
+            prev_sum = cur_sum
+        prev_line_2 = prev_line_1
+        prev_line_1 = cur_line
 print(increase_counter)
