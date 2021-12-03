ca2475eb167bc638dbb54965f2982035c12857b9
Michael <hanaauana@gmail.com>
2021-12-03 09:26:04 -0800

    Complete Day2

--- 2021/day2/day2.py
+++ 2021/day2/day2.py
@@ -2,8 +2,8 @@ input_filename = 'input.txt'
 
 # Part 1
 with open(input_filename) as input_file:
-    pos_horiz = 0;
-    pos_depth = 0;
+    pos_horiz = 0
+    pos_depth = 0
     for line in input_file:
         direction, move = line.split(' ')
         move = int(move)
@@ -14,4 +14,22 @@ with open(input_filename) as input_file:
         elif direction == 'up':
             pos_depth -= move
     pos_final = pos_depth * pos_horiz
+    print(pos_final)
+
+# Part 2
+with open(input_filename) as input_file:
+    aim = 0
+    depth = 0
+    pos_horiz = 0
+    for line in input_file:
+        direction, move = line.split(' ')
+        move = int(move)
+        if direction == 'forward':
+            pos_horiz += move
+            depth += move*aim
+        elif direction == 'down':
+            aim += move
+        elif direction == 'up':
+            aim -= move
+    pos_final = depth * pos_horiz
     print(pos_final)
