bbfec54b850a73f376b68cab1cced1af18ba4d05
Michael <hanaauana@gmail.com>
2021-12-04 11:53:34 -0800

    Finished Part 1

--- 2021/day4/day4.py
+++ 2021/day4/day4.py
@@ -1,4 +1,4 @@
-input_filename = 'test.txt'
+input_filename = 'input.txt'
 board_size = 5
 
 next_board_num = -1
@@ -33,6 +33,58 @@ with open(input_filename) as input_file:
                 # print('Reached end of last board, reset row count')
                 next_row_num = 0
 
+def transpose_board(board):
+    return [[row[i] for row in board] for i in range(len(board[0]))]
 
-print(number_draws)
-print(boards)
+
+def check_solved(board, drawn_nums):
+    if len(drawn_nums) < len(board[0]):
+        return False
+    for row in board:
+        solved = True
+        for col in row:
+            if col not in drawn_nums:
+                solved = False
+                break
+        if solved:
+            return True
+    return False
+
+
+def score_board(board, drawn_nums):
+    sum = 0
+    for row in board:
+        for col in row:
+            if col not in drawn_nums:
+                sum += col
+                print(sum)
+    final_num = drawn_nums[-1]
+    return sum*final_num
+
+
+# print(number_draws)
+# print(boards)
+
+winning_score = None
+winning_board = None
+
+drawn_nums = []
+for draw in number_draws:
+    drawn_nums.append(draw)
+    if(len(drawn_nums) >= board_size):
+        for board in boards:
+            if(check_solved(board, drawn_nums)):
+                winning_board = board
+                winning_score = score_board(board, drawn_nums)
+                break
+            else:
+                transposed_board = transpose_board(board)
+                if(check_solved(transposed_board, drawn_nums)):
+                    winning_board = board
+                    winning_score = score_board(transposed_board, drawn_nums)
+                    break
+    if winning_board:
+        break
+
+# print(winning_board)
+print(winning_score)
