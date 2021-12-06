ec757410ca92e9d2b6d21732212c6c4f72dbba72
Michael <hanaauana@gmail.com>
2021-12-06 09:33:19 -0800

    Complete day6 and clean up prints

--- 2021/day6/day6.py
+++ 2021/day6/day6.py
@@ -10,11 +10,8 @@ with open(input_filename) as input_file:
 
 initial_state = [int(i) for i in initial_string.split(',')]
 
-print(initial_state)
-
 # Part 1
 starting_state = initial_state
-# print(f'Initial state: {initial_string}')
 num_fish = len(starting_state)
 for day in range(num_days):
     new_state = list(starting_state)
@@ -24,7 +21,6 @@ for day in range(num_days):
             new_state.append(newborn_time)
         else:
             new_state[i] -= 1
-    # print(f'After {day+1} days: {new_state}')
     starting_state = new_state
     num_fish = len(starting_state)
 
@@ -40,11 +36,9 @@ for fish in starting_state:
 
 num_days = 256
 for day in range(num_days):
-    # print(f'After {day+1} days: {fish_counts}')
     new_fish = fish_counts.pop(0)
     fish_counts[incubation_time] += new_fish
     fish_counts.append(new_fish)
 
-# print(f'Final: {fish_counts}')
 num_fish = sum(fish_counts)
 print(num_fish)
