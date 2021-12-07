f60ad49f89d789f3866cdabef24bba0be61e4da2
Michael <hanaauana@gmail.com>
2021-12-07 08:36:55 -0800

    Complete day7 and clean up prints

--- 2021/day7/day7.py
+++ 2021/day7/day7.py
@@ -16,7 +16,7 @@ target_pos = median(initial_state)
 total_change = 0
 
 for crab in initial_state:
-    total_change += abs(target_pos-crab)
+    total_change += int(abs(target_pos-crab))
 
 print(total_change)
 
@@ -25,18 +25,14 @@ best_fuel_change = None
 best_pos = initial_state[0]
 
 for target in range(min(initial_state), max(initial_state)):
-    # print(f'Trying {target}')
     this_fuel_change = 0
     for crab in initial_state:
         this_crab_change = sum(range(int(abs(target-crab))+1))
-        # print(f'Moving from {crab} takes {this_crab_change}')
         this_fuel_change += this_crab_change
-    # print(this_fuel_change)
     if not best_fuel_change or this_fuel_change < best_fuel_change:
         best_fuel_change = this_fuel_change
         best_pos = target
 
-print(best_pos)
 print(best_fuel_change)
 
 
