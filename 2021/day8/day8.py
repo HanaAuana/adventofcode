c77c74bb34433842bbe6c7314e94d6095a804572
Michael <hanaauana@gmail.com>
2021-12-08 10:40:03 -0800

    Complete day8 part1

--- 2021/day8/day8.py
+++ 2021/day8/day8.py
@@ -9,3 +9,23 @@ with open(input_filename) as input_file:
 lines = [i.split('|') for i in initial_string.split('\n')]
 
 print(lines)
+
+# Part 1
+counts = [0 for i in range(10)]
+for signals, output in lines:
+
+    for value in output.split():
+        if len(value.strip()) == 2:
+            counts[1] += 1
+            continue
+        elif len(value.strip()) == 3:
+            counts[7] += 1
+            continue
+        elif len(value.strip()) == 4:
+            counts[4] += 1
+            continue
+        elif len(value.strip()) == 7:
+            counts[8] += 1
+            continue
+
+print(sum((counts[1], counts[4], counts[7], counts[8])))
