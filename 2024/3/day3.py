c5ce08f24bb76d827e623dda5005297ce93a4870
Michael Lim <hanaauana@gmail.com>
2024-12-03 12:29:44 -0800

    Part 2 solution

--- 2024/3/day3.py
+++ 2024/3/day3.py
@@ -1,6 +1,7 @@
 import re
 
-reg_pattern = r'mul\((\d+,\d+)\)'
+# reg_pattern = r'mul\((\d+,\d+)\)'
+reg_pattern = r'mul\((\d+,\d+)\)|(do\(\))|(don\'t\(\))'
 
 input_filename = 'input.txt'
 
@@ -8,5 +9,18 @@ sum
 
 with open(input_filename) as input_file:
     input_text = input_file.read()
-    matches = [item.split(',') for item in re.findall(reg_pattern, input_text)]
-    print(sum(int(item[0])*int(item[1]) for item in matches))
+    captures = re.findall(reg_pattern, input_text)
+    matches = [item for tup in captures for item in tup if item]
+    do_include = True
+    sum = 0
+    for match in matches:
+        if match == "don't()":
+            do_include = False
+        elif match == 'do()':
+            do_include = True
+        elif type(match[0] is int):
+            operands = match.split(',')
+            if do_include:
+                sum += int(operands[0]) * int(operands[1])
+
+    print(sum)
